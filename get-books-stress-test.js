55d138c69964c815adc331ec4360d1fcafe34795
rizal <rizal@lomotif.com>
2021-10-19 08:52:01 +0700

    feat: increate vus for stress test

--- get-books-stress-test.js
+++ get-books-stress-test.js
@@ -4,7 +4,7 @@ import {check, sleep} from 'k6';
 export let options = {
     stages: [
         {duration: '30s', target: 200},
-        {duration: '1m', target: 700},
+        {duration: '1m', target: 1200},
         {duration: '1m', target: 200},
         {duration: '30s', target: 0},
     ],
